201b6d38237eabdf3074af09d56df17bb10db2e1
SliceOfLiberty <sp_dimon@mail.ru>
2023-02-21 16:08:46 +0800

    module2 2task

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -9,8 +9,11 @@ function print(text) {
  * Эту функцию нужно поменять так,
  * чтобы функция sayHello работала корректно
  */
+
+
+
 function isValid(name) {
-  // ваш код...
+  return name?.trim()?.length >= 4 && !name.includes(' ');
 }
 
 function sayHello() {
